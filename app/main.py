56f2bfcce6ac1341a792430ae93c0b9e64b8c19a
Kuzmin Boris <kzmnbrs@icloud.com>
2020-10-05 02:37:51 +0000

    feat(CORS): CORS wildcard middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import authenticate
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(authenticate.router)
